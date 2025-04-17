73f052d9c1694ca782e527f8eaac8c04ad11bc4f
HWMV <hyunwoo220@gmail.com>
2025-04-17 11:54:51 +0900

    소스코드 통합에서 충돌 해결, Firebase Setting & Authentication

--- frontend/lib/features/auth/service/auth_service.dart
+++ frontend/lib/features/auth/service/auth_service.dart
@@ -1,1 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+// import '../model/user_model.dart'; 나중에 사용하게 될 때 활성화 시켜서 테스트
 
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // 현재 사용자 상태 스트림
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+
+  // 로그인
+  Future<UserCredential> signIn(String email, String password) async {
+    try {
+      return await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (e) {
+      throw Exception('로그인에 실패했습니다: $e');
+    }
+  }
+
+  // 회원가입
+  Future<UserCredential> signUp(
+      String name, String email, String password) async {
+    try {
+      final credential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // Firestore에 사용자 정보 저장
+      await _firestore.collection('users').doc(credential.user!.uid).set({
+        'name': name,
+        'email': email,
+        'uid': credential.user!.uid,
+      });
+
+      return credential;
+    } catch (e) {
+      throw Exception('회원가입에 실패했습니다: $e');
+    }
+  }
+
+  // 로그아웃
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
