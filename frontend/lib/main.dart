ba846a78452241b8ee58b7b349d2dd5e8835d59f
Platypus <jdb0108@gmail.com>
2025-04-18 12:38:18 +0900

    start page setting : login&SignUp screen

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'features/auth/ui/login_screen.dart';
 import 'features/auth/ui/signup_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const LoveGot());
 }
 
